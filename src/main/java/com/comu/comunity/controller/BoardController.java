ad585b97dd4b47b03a01a1cd4d914615a2ec24e2
musicmalgon <musicmalgon@gmail.com>
2024-10-19 23:07:10 +0900

    update controller and service

--- src/main/java/com/comu/comunity/controller/BoardController.java
+++ src/main/java/com/comu/comunity/controller/BoardController.java
@@ -1,4 +1,33 @@
 package com.comu.comunity.controller;
 
+import com.comu.comunity.dto.BoardRequestDto;
+import com.comu.comunity.dto.BoardResponseDto;
+import com.comu.comunity.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/boards")
 public class BoardController {
+
+    private final BoardService boardService;
+
+    //게시글 생성
+    @PostMapping
+    public ResponseEntity<BoardResponseDto> createBoard(@RequestBody BoardRequestDto boardRequestDto) {
+        BoardResponseDto dto = boardService.createBoard(boardRequestDto);
+    }
+    //게시글 상세조회
+
+    //게시글 전체조회
+
+    //게시글 수정
+
+    //게시글 삭제
+
 }
--- src/main/java/com/comu/comunity/service/BoardService.java
+++ src/main/java/com/comu/comunity/service/BoardService.java
@@ -1,4 +1,14 @@
 package com.comu.comunity.service;
 
+import com.comu.comunity.dto.BoardRequestDto;
+import com.comu.comunity.dto.BoardResponseDto;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BoardService {
+
+    public BoardResponseDto createBoard(BoardRequestDto boardRequestDto) {
+
+    }
 }
