d92daafabaf11bfe9442c2e212996e0aae515e08
musicmalgon <musicmalgon@gmail.com>
2024-10-19 23:06:51 +0900

    add dto

---
+++ src/main/java/com/comu/comunity/dto/BoardRequestDto.java
@@ -0,0 +1,8 @@
+package com.comu.comunity.dto;
+
+import lombok.Getter;
+
+@Getter
+public class BoardRequestDto {
+    private String contents;
+}
---
+++ src/main/java/com/comu/comunity/dto/BoardResponseDto.java
@@ -0,0 +1,9 @@
+package com.comu.comunity.dto;
+
+public class BoardResponseDto {
+    private long id;
+    private String name;
+    private String contents;
+    private String createdAt;
+    private String updatedAt;
+}
