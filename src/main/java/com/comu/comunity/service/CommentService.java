ce66fe7fa37dd24b4fd85d82a85d0883fbb98a77
jihoon <kg5860@naver.com>
2024-10-22 08:23:24 +0900

    update : CommentService create 추가

--- src/main/java/com/comu/comunity/service/CommentService.java
+++ src/main/java/com/comu/comunity/service/CommentService.java
@@ -1,4 +1,35 @@
 package com.comu.comunity.service;
 
+import com.comu.comunity.dto.CommentRequestDto;
+import com.comu.comunity.dto.CommentResponseDto;
+import com.comu.comunity.model.entity.Comment;
+import com.comu.comunity.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+
+
+    public CommentResponseDto createComment(CommentRequestDto requestDto) {
+
+        // RequestDto -> Entity
+        Comment comment = new Comment(requestDto);
+
+        // DB 저장
+        Comment saveComment = commentRepository.save(comment);
+
+        // Entity -> ResponseDto
+        CommentResponseDto commentResponseDto = new CommentResponseDto(comment);
+        return commentResponseDto;
+
+    }
+
+
 }
