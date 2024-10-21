ef4276fc0d239f85a98f4706527b51bbdefadd9f
gebeen-dev <rrdd1207@gmail.com>
2024-10-21 23:10:00 +0900

    update : MemberRepository 추가

--- src/main/java/com/comu/comunity/repository/MemberRepository.java
+++ src/main/java/com/comu/comunity/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.comu.comunity.repository;
 import com.comu.comunity.model.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
