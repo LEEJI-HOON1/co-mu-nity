a12fc094c6fb034cc175133712bd2fee2877e84b
jihoon <kg5860@naver.com>
2024-10-22 07:55:37 +0900

    update : Controller get 추가

--- src/main/java/com/comu/comunity/controller/CommentController.java
+++ src/main/java/com/comu/comunity/controller/CommentController.java
@@ -1,4 +1,26 @@
 package com.comu.comunity.controller;
 
+import com.comu.comunity.dto.CommentRequestDto;
+import com.comu.comunity.dto.CommentResponseDto;
+import com.comu.comunity.service.CommentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/boards/{boardId}")
 public class CommentController {
+
+    private  final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;}
+
+
+    @PostMapping("/comments")
+    public CommentResponseDto createComment (@RequestBody CommentRequestDto requestDto){
+        //현재 로그인한 사용자의 정보를 가져옴
+        return commentService.createComment(requestDto);
+    }
 }
