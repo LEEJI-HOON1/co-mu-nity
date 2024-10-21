9a3d4bdecfe5400d97b78bc9395aa1b1558a558e
gebeen-dev <rrdd1207@gmail.com>
2024-10-21 23:25:20 +0900

    update : JwtTokenProvider getMemberId로 변경

--- src/main/java/com/comu/comunity/auth/JwtTokenProvider.java
+++ src/main/java/com/comu/comunity/auth/JwtTokenProvider.java
@@ -1,10 +1,12 @@
 package com.comu.comunity.auth;
 
 import com.comu.comunity.model.entity.Member;
+import com.comu.comunity.repository.MemberRepository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
@@ -14,10 +16,17 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import java.util.Date;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class JwtTokenProvider {
 
+    private final MemberRepository memberRepository;
+
+//토큰과 관련된 기능 틀래스
+
+    //토크을 가지오 오는 키
     private final String secretKey = "TTTOPSECRETKEYTOPSECRETKEYTOPSECRETKEYTOPSECRETKEYTOPSECRETKEYOPSECRETKEYTTOPSECRETKEYTOPSECRETKEYTOPSECRETKEYTOPSECRETKEYTOPSECRETKEYOPSECRETKEYTTOPSECRETKEYTOPSECRETKEYTOPSECRETKEYTOPSECRETKEYTOPSECRETKEYOPSECRETKEYTOPSECRETKEYTOPSECRETKEYTOPSECRETKEYTOPSECRETKEYTOPSECRETKEYOPSECRETKEY";
+    //유효시간
     private final long validityInMilliseconds = 3600000; // 1시간
 
     public String createToken(Member member) {
@@ -55,13 +64,17 @@ public class JwtTokenProvider {
         }
     }
 
-    public String getEmail() {
+    public Long getMemberId() {
         HttpServletRequest request = getCurrentHttpRequest();  // 현재 요청 객체 가져오기
         if (request != null) {
             String bearerToken = request.getHeader("Authorization");
             if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
                 String token = extractJwtToken(bearerToken);
-                return getEmailFromToken(token);  // 토큰에서 이메일 추출
+                 String email = getEmailFromToken(token);  // 토큰에서 이메일 추출
+               Member member =  memberRepository.findByEmail(email)
+                        .orElseThrow(() -> new RuntimeException("member not found"));
+
+                return member.getId();
             }
         }
         return null;
