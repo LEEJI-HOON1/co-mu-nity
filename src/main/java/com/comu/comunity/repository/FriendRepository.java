b41f458e4d495265892e7ed571b152b8efa2dfe9
hubaek <zjfl3122@naver.com>
2024-10-22 16:36:11 +0900

    update : JpaRepository상속, existsByFromMemberIdAndToMemberId 메서드 추가 - existsByFromMemberIdAndToMemberId : 요청id인 사람이 요청받는 Id가 이미 존재하는지 확인하는 메서드

--- src/main/java/com/comu/comunity/repository/FriendRepository.java
+++ src/main/java/com/comu/comunity/repository/FriendRepository.java
@@ -1,4 +1,13 @@
 package com.comu.comunity.repository;
 
-public class FriendRepository {
+import com.comu.comunity.model.entity.Friend;
+import com.comu.comunity.model.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+
+    // a가 b를 팔로우 하는지 확인
+    boolean existsByFromMemberIdAndToMemberId(Long fromMemberId, Long toMemberId);
 }
