c2ef3eb93e3ac578743702ebb890215d9086f74c
hubaek <zjfl3122@naver.com>
2024-10-22 16:05:02 +0900

    update : FriendController follow 기능 추가 - follow : 임시로 fromMemberId를 받고, toMemberId 두개의 id로 follow 기능 완성

--- src/main/java/com/comu/comunity/controller/FriendController.java
+++ src/main/java/com/comu/comunity/controller/FriendController.java
@@ -1,4 +1,29 @@
 package com.comu.comunity.controller;
 
+import com.comu.comunity.dto.FriendResponseDto;
+import com.comu.comunity.service.FriendService;
+import com.comu.comunity.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class FriendController {
+
+    private final FriendService friendService;
+    private final MemberService memberService;
+
+    // Todo
+    //  로그인기능 완료후, fromMemberId는 토큰으로 구분해주기
+    // 팔로잉하기 (친구맺기)
+    @PostMapping("/members/{fromMemberId}/follow/{toMemberId}")
+    public ResponseEntity<FriendResponseDto> follow(@PathVariable Long fromMemberId, @PathVariable Long toMemberId) {
+        String message = friendService.follow(fromMemberId, toMemberId);
+        FriendResponseDto friendResponseDto = new FriendResponseDto(message, fromMemberId, toMemberId);
+        return ResponseEntity.ok(friendResponseDto);
+    }
+
 }
