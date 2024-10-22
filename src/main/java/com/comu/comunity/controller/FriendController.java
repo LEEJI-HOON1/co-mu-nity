2c9ebf72fee5b89662d2230309c803a6708991cb
hubaek <zjfl3122@naver.com>
2024-10-22 18:50:27 +0900

    update : FriendController 반환값 수정 - 불필요한 message 삭제 - follow메서드 타입을 Friend Entity로 변경하고, Friend Entity의 id를 넣어서 Response로 반환

--- src/main/java/com/comu/comunity/controller/FriendController.java
+++ src/main/java/com/comu/comunity/controller/FriendController.java
@@ -1,6 +1,7 @@
 package com.comu.comunity.controller;
 
 import com.comu.comunity.dto.FriendResponseDto;
+import com.comu.comunity.model.entity.Friend;
 import com.comu.comunity.service.FriendService;
 import com.comu.comunity.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -21,8 +22,9 @@ public class FriendController {
     // 팔로잉하기 (친구맺기)
     @PostMapping("/members/{fromMemberId}/follow/{toMemberId}")
     public ResponseEntity<FriendResponseDto> follow(@PathVariable Long fromMemberId, @PathVariable Long toMemberId) {
-        String message = friendService.follow(fromMemberId, toMemberId);
-        FriendResponseDto friendResponseDto = new FriendResponseDto(message, fromMemberId, toMemberId);
+        Friend followFriend = friendService.follow(fromMemberId, toMemberId);
+
+        FriendResponseDto friendResponseDto = new FriendResponseDto(followFriend.getId(), fromMemberId, toMemberId);
         return ResponseEntity.ok(friendResponseDto);
     }
 
