548ddf5b059213bdc3c206b42e3dbfb46cee1344
hubaek <zjfl3122@naver.com>
2024-10-23 01:39:23 +0900

    update : FriendRepository 쿼리메서드 추가 - findByFromMemberId - findByToMemberId

--- src/main/java/com/comu/comunity/repository/FriendRepository.java
+++ src/main/java/com/comu/comunity/repository/FriendRepository.java
@@ -1,13 +1,19 @@
 package com.comu.comunity.repository;
 
 import com.comu.comunity.model.entity.Friend;
-import com.comu.comunity.model.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 
-    // a가 b를 팔로우 하는지 확인
+    // a가 b를 팔로우 하는지 확인 - 팔로잉 여부 확인
     boolean existsByFromMemberIdAndToMemberId(Long fromMemberId, Long toMemberId);
+
+    // fromMemberId
+    List<Friend> findByFromMemberId(Long fromMemberId);
+
+    // ToMemberId의 Friend 
+    List<Friend> findByToMemberId(Long toMemberId);
 }
+
