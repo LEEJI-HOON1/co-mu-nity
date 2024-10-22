56928323795d7b0bc76aaf7f524240df15d82795
hubaek <zjfl3122@naver.com>
2024-10-23 01:13:21 +0900

    update : FriendService 팔로잉, 팔로워 기능 추가 - getFollowings : findById로 해당 멤버가 존재하는지 확인 후 fromMemberId의 List를 찾고, toMember의 정보를 MemberResponsDto에 담아서 반환한다. - getFollowers : findById로 해당 멤버가 존재하는지 확인 후 toMemberId의 List를 찾고, fromMember의 정보를 MemberResponsDto에 담아서 반환한다.

--- src/main/java/com/comu/comunity/service/FriendService.java
+++ src/main/java/com/comu/comunity/service/FriendService.java
@@ -1,12 +1,16 @@
 package com.comu.comunity.service;
 
-import com.comu.comunity.dto.FriendResponseDto;
+import com.comu.comunity.dto.MemberResponseDto;
 import com.comu.comunity.model.entity.Friend;
 import com.comu.comunity.model.entity.Member;
 import com.comu.comunity.repository.FriendRepository;
 import com.comu.comunity.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +22,7 @@ public class FriendService {
     // Todo
     //  토큰생성시, fromMemberId는 로그인한 사용자로 변경해야함
     // fromMember가 toMember 팔로우
+    @Transactional
     public Friend follow(Long fromMemberId, Long toMemberId) {
         // 자기 자신 팔로우 안됨
         if (fromMemberId.equals(toMemberId)) {
@@ -30,14 +35,38 @@ public class FriendService {
         Member fromMember = memberRepository.findById(fromMemberId).orElseThrow(RuntimeException::new);
         Member toMember = memberRepository.findById(toMemberId).orElseThrow(RuntimeException::new);
 
+
+
         Friend friend = new Friend(fromMember, toMember);
         return friendRepository.save(friend);
     }
 
-    // 팔로우중인지 확인하기
-    public boolean isFollowing(Long fromMemberId, Long toMemberId){
-        return friendRepository.existsByFromMemberIdAndToMemberId(fromMemberId, toMemberId);
+    public List<MemberResponseDto> getFollowings(Long fromMemberId) {
+        // 해당 멤버가 존재하는지 확인
+        memberRepository.findById(fromMemberId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
+        // fromMember의 팔로잉 목록을 조회한다.
+        List<Friend> followings = friendRepository.findByFromMemberId(fromMemberId);
+        // 팔로잉 정보가 필요할땐 get필드 추가
+        return followings.stream()
+                .map(friend -> new MemberResponseDto(friend.getToMember().getId(), friend.getToMember().getName()))
+                .collect(Collectors.toList());
     }
 
+    public List<MemberResponseDto> getFollowers(Long toMemberId) {
+        memberRepository.findById(toMemberId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
+        List<Friend> followers = friendRepository.findByToMemberId(toMemberId);
+        // 팔로잉 정보가 필요할땐 get필드 추가
+        return followers.stream()
+                .map(friend -> new MemberResponseDto(friend.getFromMember().getId(), friend.getFromMember().getName()))
+                .collect(Collectors.toList());
+    }
+
+    // 팔로우중인지 확인하기
+    private boolean isFollowing(Long fromMemberId, Long toMemberId){
+        return friendRepository.existsByFromMemberIdAndToMemberId(fromMemberId, toMemberId);
+    }
 }
