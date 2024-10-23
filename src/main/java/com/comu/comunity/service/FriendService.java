881eff91bf26be7be514f9c98968ce5b914f0ccb
hubaek <zjfl3122@naver.com>
2024-10-24 01:44:26 +0900

    update : Friend unfollow 로그인한 사용자 로직 추가 - loginedMember 파라미터로 받아와서, getId해서 fromMemberId를 loginedMember로 변경

--- src/main/java/com/comu/comunity/service/FriendService.java
+++ src/main/java/com/comu/comunity/service/FriendService.java
@@ -20,10 +20,6 @@ public class FriendService {
     private final FriendRepository friendRepository;
     private final MemberRepository memberRepository;
 
-    // Todo
-    //  토큰생성시, fromMemberId는 로그인한 사용자로 변경해야함
-    // fromMember가 toMember 팔로우
-
     @Transactional
     public FriendResponseDto follow(Member loginedMember, Long toMemberId) {
 
@@ -45,14 +41,27 @@ public class FriendService {
         return new FriendResponseDto(friend.getId(), loginedMemberId, toMemberId);
     }
 
+    public FriendResponseDto unfollow(Member loginedMember, Long toMemberId) {
+
+        Long loginedMemberId = loginedMember.getId();
+
+        if (loginedMemberId.equals(toMemberId)) {
+            throw new RuntimeException("자기 자신을 unfollow 할 수 없습니다");
+        }
+        // 팔로우 관계 조회
+        Friend friend = friendRepository.findByFromMemberIdAndToMemberId(loginedMemberId, toMemberId)
+                .orElseThrow(() -> new IllegalArgumentException("팔로우가 되어 있지 않습니다."));
+
+        friendRepository.delete(friend);
+        return new FriendResponseDto(friend.getId(), loginedMemberId, toMemberId);
+    }
+
     public List<MemberResponseDto> getFollowings(Long fromMemberId) {
         // 해당 멤버가 존재하는지 확인
         memberRepository.findById(fromMemberId)
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
-        // fromMember의 팔로잉 목록을 조회한다.
         List<Friend> followings = friendRepository.findByFromMemberId(fromMemberId);
-        // 팔로잉 정보가 필요할땐 get필드 추가
         return followings.stream()
                 .map(friend -> new MemberResponseDto(friend.getToMember().getId(), friend.getToMember().getName()))
                 .collect(Collectors.toList());
@@ -63,26 +72,11 @@ public class FriendService {
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
         List<Friend> followers = friendRepository.findByToMemberId(toMemberId);
-        // 팔로잉 정보가 필요할땐 get필드 추가
         return followers.stream()
                 .map(friend -> new MemberResponseDto(friend.getFromMember().getId(), friend.getFromMember().getName()))
                 .collect(Collectors.toList());
     }
 
-    public FriendResponseDto unfollow(Long fromMemberId, Long toMemberId) {
-        if (fromMemberId.equals(toMemberId)) {
-            throw new RuntimeException("자기 자신을 unfollow 할 수 없습니다");
-        }
-        // 팔로우 관계 조회
-        Friend friend = friendRepository.findByFromMemberIdAndToMemberId(fromMemberId, toMemberId)
-                .orElseThrow(() -> new IllegalArgumentException("팔로우가 되어 있지 않습니다."));
-
-       friendRepository.delete(friend);
-       return new FriendResponseDto(friend.getId(), fromMemberId, toMemberId);
-
-    }
-
-    // 팔로우중인지 확인하기
     private boolean isFollowing(Long fromMemberId, Long toMemberId){
         return friendRepository.existsByFromMemberIdAndToMemberId(fromMemberId, toMemberId);
     }
